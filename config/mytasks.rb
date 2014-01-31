659c8062d62e811218802a1abe813695e5d4191b
pasupulaphani <pasupulaphani@gmail.com>
2014-01-31 22:27:24 +0000

    try without capistrano ask

--- config/mytasks.rb
+++ config/mytasks.rb
@@ -16,7 +16,7 @@ end
 
 namespace :git do
 	task :commit do
-		run_local("echo 'Starting git............................................................'")
+		run_local("echo 'Starting git task :commit............................................................'")
     # Check for any local changes that haven't been committed
     status = %x(git status --porcelain).chomp
    	puts "\nLocal git status : #{status}"
@@ -24,16 +24,20 @@ namespace :git do
    	puts "\nChanges stagged"
    	status = %x(git add . )
 
-		puts "Committing local changes"
-		puts "Enter commit message \n"
-		#set(:meg, Capistrano::CLI.ui.ask("Enter commit message: ") )
-		ask :msg, STDIN.gets.chomp rescue nil
-   	run_local("git commit -m '#{:msg}'")
+		puts "\nCommitting local changes"
+		puts "\nEnter commit message :"
+		msg = STDIN.gets.chomp rescue nil
+   	run_local("git commit -m '#{msg}'")
  
     # Check we are on the master branch, so we can't forget to merge before deploying
     branch = %x(git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \\(.*\\)/\\1/').chomp
-    if branch != "master" && !ENV["IGNORE_BRANCH"]
-      raise Capistrano::Error, "Not on master branch (set IGNORE_BRANCH=1 to ignore)"
+    if branch != "master" && ENV["CHECK_BRANCH"]
+      raise Capistrano::Error, "Not on master branch (set CHECK_BRANCH=0 to ignore)"
+    end
+
+    # Push the changes
+    if ! system "git push #{fetch(:repository)} master"
+      raise Capistrano::Error, "Failed to push changes to #{fetch(:repository)}"
     end
 	end
 end
