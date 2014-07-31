0eca10bb9bbd6974509536f1ef920d5cc755dfda
pasupulaphani <pasupulaphani@gmail.com>
2014-07-31 02:12:19 +0100

    change of server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
 
-server 'ec2-174-129-242-238.compute-1.amazonaws.com', user: 'deploy', roles: %w{web app}, port: 22
+server '178.62.31.145', user: 'deploy', roles: %w{web app}, port: 22
