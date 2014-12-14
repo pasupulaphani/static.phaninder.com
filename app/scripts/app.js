8d996608484b3da7946dcae35f8fe08fab229ff3
pasupulaphani <pasupulaphani@gmail.com>
2014-12-14 19:04:05 +0000

    some changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,54 +9,60 @@
  * Main module of the application.
  */
 angular
-  .module('angappApp', [
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'socialsharing'
-  ])
+    .module('angappApp', [
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'socialsharing'
+    ])
 
-  .constant('restEndPoint', 'http://localhost:3000')
+.constant('restEndPoint', 'http://localhost:3000')
 
-  .run(function($rootScope) {
+.run(function($rootScope) {
 
     // initialize foundation
     Foundation.global.namespace = '';
     $(document).foundation();
 
+    $rootScope.site = {
+        name: "phaninder.com",
+        url: encodeURIComponent(document.URL),
+        short_url: document.URL
+    };
+
     // set some common content
     $rootScope.me = {
-      image : 'http://www.gravatar.com/avatar/da8ad3d7a783fda9082894427e6be2a9.png',
-      description : 'Hi there! I\'m Phani. I live in London. I am a full stack developer who specializes in creating dynamic and beautiful web apps. I currently spend more time in developing new automation techniques for automating project life cycles of my mobile apps.'
+        image: 'http://www.gravatar.com/avatar/da8ad3d7a783fda9082894427e6be2a9.png',
+        description: 'Hi there! I\'m Phani. I live in London. I am a full stack developer who specializes in creating dynamic and beautiful web apps. I currently spend more time in developing new automation techniques for automating project life cycles of my mobile apps.'
     };
-  })
-  
-  .config(function ($routeProvider, $fbProvider, $twtProvider) {
+})
+
+.config(function($routeProvider, $fbProvider, $twtProvider) {
     $routeProvider
-      .when('/posts', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/posts/:id/:seo_title?', {
-        templateUrl: 'views/post.html',
-        controller: 'PostCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/post.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/contact', {
-        templateUrl: 'views/post.html',
-        controller: 'ContactCtrl'
-      })
-      .otherwise({
-        redirectTo: '/posts'
-      });
+        .when('/posts', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .when('/posts/:id/:seo_title?', {
+            templateUrl: 'views/post.html',
+            controller: 'PostCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'views/post.html',
+            controller: 'AboutCtrl'
+        })
+        .when('/contact', {
+            templateUrl: 'views/post.html',
+            controller: 'ContactCtrl'
+        })
+        .otherwise({
+            redirectTo: '/posts'
+        });
 
     $fbProvider.init(515239655250335);
 
     $twtProvider.setConfig({
         trim_text: true
     });
-  });
+});
--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -10,7 +10,7 @@
 angular.module('angappApp')
     .controller(
         'PostCtrl',
-        function($scope, $routeParams, $location, post, utils) {
+        function($scope, $routeParams, $location, post, utils, $twt) {
             $scope.posts = post.query({
                 id: $routeParams.id
             });
@@ -19,14 +19,24 @@ angular.module('angappApp')
                 utils.markUp($scope.posts);
 
                 // instead use route resolve
-                $location.path('/posts/'+$scope.posts[0]._id+'/'+$scope.posts[0].seo_url)
+                $location.path('/posts/' + $scope.posts[0]._id + '/' + $scope.posts[0].seo_url)
             });
 
-            $scope.findMe = 'social_sharing';
+            $scope.share = {
+                tpl: 'social_sharing',
+                twt: function() {
+                    $twt.intent('tweet', {
+                        text: 'Adventures at NodeCoptor',
+                        url: 'http://localhost:9000/#/posts/sdfg3/undefined',
+                        hashtags: 'phaninder.com'
+                    });
+                }
+            };
+
         })
     .controller(
         'AboutCtrl',
-        function($scope, about) {
+        function($scope, utils, about) {
 
             $scope.posts = about.query({
                 id: 'about'
@@ -36,11 +46,13 @@ angular.module('angappApp')
                 utils.markUp($scope.posts);
             });
 
-            $scope.findMe = 'social_networking';
+            $scope.share = {
+                tpl: 'social_sharing'
+            };
         })
     .controller(
         'ContactCtrl',
-        function($scope, contact) {
+        function($scope, utils, contact) {
 
             $scope.posts = contact.query({
                 id: 'contact'
@@ -50,5 +62,7 @@ angular.module('angappApp')
                 utils.markUp($scope.posts);
             });
 
-            $scope.findMe = 'social_networking';
+            $scope.share = {
+                tpl: 'social_networking'
+            };
         });
