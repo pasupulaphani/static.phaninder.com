5da4750478045df2df45adb36ff1f39da6221d68
pasupulaphani <pasupulaphani@gmail.com>
2014-12-25 00:38:51 +0000

    use ssh agent

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
 
-server '134.213.153.219', user: 'root',  password: fetch(:password), roles: %w{web app}, port: 22
+server '134.213.153.219', user: 'root', roles: %w{web app}, port: 22
