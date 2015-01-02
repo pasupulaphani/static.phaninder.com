3622f5b72edc1722461e7284559a4466499dc80e
pasupulaphani <pasupulaphani@gmail.com>
2015-01-02 18:17:37 +0000

    success with root

--- config/deploy/vm.rb
+++ config/deploy/vm.rb
@@ -2,4 +2,4 @@ set :stage, :vm
 
 set :app_mode, 'vm'
 
-server '127.0.0.1', user: 'deploy', roles: %w{web app}, port: 2222
+server '127.0.0.1', user: 'root', password: 'vagrant', roles: %w{web app}, port: 2222
